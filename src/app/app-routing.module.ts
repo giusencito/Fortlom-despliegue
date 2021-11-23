279d93a892645cfa0c94b9de1d152b2da82e7e35
giusencito <66499088+giusencito@users.noreply.github.com>
2021-11-23 00:07:25 -0500

    Revert "cambios finales"
    This reverts commit 17a2fffe9ff8ac3532d6dd83efcfd3e616eddeea.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,7 @@ import { ArtistRegisterComponent } from './ArtistRegister/ArtistRegister.compone
 import { LoginComponent } from './Login/Login.component';
 import { SkeletonComponent } from './skeleton/skeleton.component';
 import { FanaticForumComponent } from './Fanatic-Forum/Fanatic-Forum.component';
-import { EventFanaticComponent } from './event-fanatic/event-fanatic.component';
-import { ArtistListComponent } from './artist-list/artist-list.component';
+
 
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -18,38 +17,22 @@ import { ArtistForumComponent } from './artist-forum/artist-forum.component';
 import { HomeFanaticComponent } from './HomeFanatic/HomeFanatic.component';
 import { Posts2Component } from "./posts2/posts2.component";
 import {CommentTableComponent} from "./comment-table/comment-table.component";
-import { ConfigurationComponent } from './configuration/configuration.component';
-import { ConfigurationFanaticComponent } from './configuration-fanatic/configuration-fanatic.component';
-import { ArtistForumsComponent } from './ArtistForums/ArtistForums.component';
+
 const routes: Routes = [
-
-  {path:'',component:LoginComponent},
-  {path:'login',component:LoginComponent},
-  {path : 'Home', component: HomeComponent},
-  {path:'Event',component:EventComponent},
-  {path:'Posts',component:PostPageComponent},
+{path:'',component:LoginComponent},
+{path:'login',component:LoginComponent},
+{path : 'Home', component: HomeComponent},
+{path:'Event',component:EventComponent},
+{path:'Posts',component:Posts2Component},
   {path:'Comments',component:CommentTableComponent},
-  {path:'artistforum',component:ArtistForumComponent},
-  {path:'registerartist',component:ArtistRegisterComponent},
-  {path:'registerfanatic',component:FanaticRegisterComponent},
-  {path:'HomeArtist/:id',component:HomeArtistComponent},
-  {path:'HomeArtist/:id/ArtistForum',component:ArtistForumsComponent},
-  {path:'HomeArtist/:id/ArtistForum/CreateForum',component:FanaticForumCreateComponent},
-  {path:'HomeArtist/:id/ArtistForum/ForumPage/:forumid',component:ForumPageComponent},
-  {path:'HomeFanatic/:id/ConfigureFanatic',component:ConfigurationFanaticComponent},
-  {path:'HomeFanatic/:id',component:HomeFanaticComponent},
-  {path:'HomeFanatic/:id/FanaticForum',component:FanaticForumComponent},
-  {path:'HomeFanatic/:id/FanaticForum/CreateForum',component:FanaticForumCreateComponent},
-  {path:'HomeFanatic/:id/FanaticForum/ForumPage/:forumid',component:ForumPageComponent},
-  {path:'HomeFanatic/:id/Event',component:EventFanaticComponent},
-  {path:'HomeArtist/:id/Event',component:EventComponent},
-  {path: 'HomeArtist/:id/ConfigureArtist',component:ConfigurationComponent},
-  {path: 'artist/:artistid/posts', component:PostPageComponent},
-  {path: 'HomeArtist/:id/posts',component:PostPageComponent},
-  {path: 'HomeFanatic/:id/posts',component:PostPageComponent},
-  {path:'Comments',component:CommentTableComponent},
-  {path:'HomeFanatic/:id/artists',component:ArtistListComponent},
-
+{path:'artistforum',component:ArtistForumComponent},
+{path:'registerartist',component:ArtistRegisterComponent},
+{path:'registerfanatic',component:FanaticRegisterComponent},
+{path:'HomeArtist/:artistid',component:HomeArtistComponent},
+{path:'HomeFanatic/:fanaticid',component:HomeFanaticComponent},
+{path:'HomeFanatic/:fanaticid/FanaticForum',component:FanaticForumComponent},
+{path:'HomeFanatic/:fanaticid/FanaticForum/CreateForum',component:FanaticForumCreateComponent},
+{path:'HomeFanatic/:fanaticid/FanaticForum/ForumPage/:forumid',component:ForumPageComponent}
 ];
 
 @NgModule({
